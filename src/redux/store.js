68b9f96ed94eab3f1449d14d979a9ec94bbff292
suyalamritanshu <amritanshu.suyall2@gmail.com>
2022-04-30 23:30:37 +0530

    Added things

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-// import { createStore, applyMiddleware } from 'redux';
-// import { rootReducer } from './reducers/rootReducer';
-// import { composeWithDevTools } from 'redux-devtools-extension';
-// import reduxThunk from 'redux-thunk';
-
-// export const store = createStore(
-//   rootReducer, composeWithDevTools(applyMiddleware(reduxThunk))
-// )
--- src/utils/validateForm.js
+++
@@ -1,5 +0,0 @@
-export const validateProfile = ({ name }) => {
-  const errors = {};
-  if (!name) errors.name = 'Enter your name';
-  return errors;
-}
