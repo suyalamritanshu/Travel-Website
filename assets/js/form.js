a5826fc6d656b41b1b4fc19f6deb50f04ee78034
suyalamritanshu <amritanshu.suyall2@gmail.com>
2022-05-02 15:20:20 +0530

    Added form

---
+++ assets/js/form.js
@@ -0,0 +1,29 @@
+base_url = 'https://travel-backend-personal.herokuapp.com';
+
+function loginUser() {
+    var data = JSON.stringify({
+        "from": document.getElementById("book_from").value,
+        "to": document.getElementById("book_to").value,
+        "date": document.getElementById("book_date").value,
+
+    });
+
+    var config = {
+        method: 'post',
+        url:  'https://travel-backend-personal.herokuapp.com/api/tour',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: data
+    };
+
+    axios(config)
+        .then(function(response) {
+            console.log(JSON.stringify(response.data));
+            location.replace('/book.html');
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+
+}
