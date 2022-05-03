f212b849280c5daf9cbc757371394b98c964d92f
suyalamritanshu <amritanshu.suyall2@gmail.com>
2022-05-04 01:16:47 +0530

    fixes

--- assets/js/form.js
+++ assets/js/form.js
@@ -55,3 +55,31 @@ function signUpUser() {
         });
 
 }
+
+function signinUser() {
+    var data = JSON.stringify({
+        "username": document.getElementById("first_name").value,
+        "password": document.getElementById("pass").value,
+
+
+    });
+
+    var config = {
+        method: 'post',
+        url:  'https://travel-backend-personal.herokuapp.com/api/signup',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: data
+    };
+
+    axios(config)
+        .then(function(response) {
+            console.log(JSON.stringify(response.data));
+            location.replace('/main.html');
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+
+}
